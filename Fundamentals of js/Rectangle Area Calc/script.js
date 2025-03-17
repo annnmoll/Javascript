515093728451cdb3121d5a6686d7019757b6d72e
Anmol Ahuja <anmolahuja250@gmail.com>
2025-03-17 11:38:52 +0530

    Rectangle area calculator

---
+++ Fundamentals of js/Rectangle Area Calc/script.js
@@ -0,0 +1,9 @@
+function main(len,wid){
+    let result;
+    //Write the program to calculate the area of rectangle using len and wid as length and width of rectangle
+    let area = len * wid ;
+    //Assign your final value to result.
+    result = area ;
+    //Do not alter any else code.
+    return result;
+    }
